0321e3157c8ffb05c376ad02955c516ea82c06df
mike finochio <mfinochio26@gmail.com>
2021-03-28 18:27:10 -0400

    Milestone2

--- InventoryProgram/Form1.Designer.cs
+++ InventoryProgram/Form1.Designer.cs
@@ -41,21 +41,31 @@ namespace InventoryProgram
             this.jacketAns = new System.Windows.Forms.TextBox();
             this.hatAns = new System.Windows.Forms.TextBox();
             this.shoeAns = new System.Windows.Forms.TextBox();
-            this.saveShirt = new System.Windows.Forms.Button();
-            this.savePant = new System.Windows.Forms.Button();
-            this.saveJacket = new System.Windows.Forms.Button();
-            this.saveHat = new System.Windows.Forms.Button();
             this.saveShoe = new System.Windows.Forms.Button();
             this.inventoryBox = new System.Windows.Forms.Label();
             this.viewButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.saveHat = new System.Windows.Forms.Button();
+            this.saveJacket = new System.Windows.Forms.Button();
+            this.savePant = new System.Windows.Forms.Button();
+            this.saveShirt = new System.Windows.Forms.Button();
+            this.shirtsBox = new System.Windows.Forms.GroupBox();
+            this.pantsBox = new System.Windows.Forms.GroupBox();
+            this.jacketsBox = new System.Windows.Forms.GroupBox();
+            this.hatsBox = new System.Windows.Forms.GroupBox();
+            this.shoesBox = new System.Windows.Forms.GroupBox();
+            this.shirtsBox.SuspendLayout();
+            this.pantsBox.SuspendLayout();
+            this.jacketsBox.SuspendLayout();
+            this.hatsBox.SuspendLayout();
+            this.shoesBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // inventoryIntro
             // 
             this.inventoryIntro.AutoSize = true;
             this.inventoryIntro.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.inventoryIntro.Location = new System.Drawing.Point(220, 34);
+            this.inventoryIntro.Location = new System.Drawing.Point(347, 9);
             this.inventoryIntro.Name = "inventoryIntro";
             this.inventoryIntro.Size = new System.Drawing.Size(353, 54);
             this.inventoryIntro.TabIndex = 0;
@@ -65,7 +75,7 @@ namespace InventoryProgram
             // 
             this.inventoryQuestion.AutoSize = true;
             this.inventoryQuestion.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.inventoryQuestion.Location = new System.Drawing.Point(12, 134);
+            this.inventoryQuestion.Location = new System.Drawing.Point(12, 88);
             this.inventoryQuestion.Name = "inventoryQuestion";
             this.inventoryQuestion.Size = new System.Drawing.Size(445, 41);
             this.inventoryQuestion.TabIndex = 1;
@@ -75,7 +85,7 @@ namespace InventoryProgram
             // 
             this.shirtQuest.AutoSize = true;
             this.shirtQuest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shirtQuest.Location = new System.Drawing.Point(12, 204);
+            this.shirtQuest.Location = new System.Drawing.Point(6, 22);
             this.shirtQuest.Name = "shirtQuest";
             this.shirtQuest.Size = new System.Drawing.Size(98, 41);
             this.shirtQuest.TabIndex = 2;
@@ -85,7 +95,7 @@ namespace InventoryProgram
             // 
             this.jacketQuest.AutoSize = true;
             this.jacketQuest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.jacketQuest.Location = new System.Drawing.Point(12, 318);
+            this.jacketQuest.Location = new System.Drawing.Point(6, 19);
             this.jacketQuest.Name = "jacketQuest";
             this.jacketQuest.Size = new System.Drawing.Size(119, 41);
             this.jacketQuest.TabIndex = 3;
@@ -95,7 +105,7 @@ namespace InventoryProgram
             // 
             this.pantQuest.AutoSize = true;
             this.pantQuest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.pantQuest.Location = new System.Drawing.Point(12, 258);
+            this.pantQuest.Location = new System.Drawing.Point(7, 22);
             this.pantQuest.Name = "pantQuest";
             this.pantQuest.Size = new System.Drawing.Size(96, 41);
             this.pantQuest.TabIndex = 4;
@@ -105,7 +115,7 @@ namespace InventoryProgram
             // 
             this.hatQuest.AutoSize = true;
             this.hatQuest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.hatQuest.Location = new System.Drawing.Point(12, 378);
+            this.hatQuest.Location = new System.Drawing.Point(6, 20);
             this.hatQuest.Name = "hatQuest";
             this.hatQuest.Size = new System.Drawing.Size(84, 41);
             this.hatQuest.TabIndex = 5;
@@ -115,7 +125,7 @@ namespace InventoryProgram
             // 
             this.showQuest.AutoSize = true;
             this.showQuest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.showQuest.Location = new System.Drawing.Point(12, 444);
+            this.showQuest.Location = new System.Drawing.Point(6, 22);
             this.showQuest.Name = "showQuest";
             this.showQuest.Size = new System.Drawing.Size(105, 41);
             this.showQuest.TabIndex = 6;
@@ -123,7 +133,7 @@ namespace InventoryProgram
             // 
             // shirtAns
             // 
-            this.shirtAns.Location = new System.Drawing.Point(178, 218);
+            this.shirtAns.Location = new System.Drawing.Point(157, 36);
             this.shirtAns.Name = "shirtAns";
             this.shirtAns.Size = new System.Drawing.Size(210, 27);
             this.shirtAns.TabIndex = 7;
@@ -131,7 +141,7 @@ namespace InventoryProgram
             // 
             // pantAns
             // 
-            this.pantAns.Location = new System.Drawing.Point(178, 272);
+            this.pantAns.Location = new System.Drawing.Point(158, 36);
             this.pantAns.Name = "pantAns";
             this.pantAns.Size = new System.Drawing.Size(210, 27);
             this.pantAns.TabIndex = 8;
@@ -139,7 +149,7 @@ namespace InventoryProgram
             // 
             // jacketAns
             // 
-            this.jacketAns.Location = new System.Drawing.Point(178, 332);
+            this.jacketAns.Location = new System.Drawing.Point(157, 33);
             this.jacketAns.Name = "jacketAns";
             this.jacketAns.Size = new System.Drawing.Size(210, 27);
             this.jacketAns.TabIndex = 9;
@@ -147,7 +157,7 @@ namespace InventoryProgram
             // 
             // hatAns
             // 
-            this.hatAns.Location = new System.Drawing.Point(178, 392);
+            this.hatAns.Location = new System.Drawing.Point(158, 34);
             this.hatAns.Name = "hatAns";
             this.hatAns.Size = new System.Drawing.Size(210, 27);
             this.hatAns.TabIndex = 10;
@@ -155,110 +165,172 @@ namespace InventoryProgram
             // 
             // shoeAns
             // 
-            this.shoeAns.Location = new System.Drawing.Point(178, 458);
+            this.shoeAns.Location = new System.Drawing.Point(158, 34);
             this.shoeAns.Name = "shoeAns";
             this.shoeAns.Size = new System.Drawing.Size(210, 27);
             this.shoeAns.TabIndex = 11;
             this.shoeAns.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // saveShirt
-            // 
-            this.saveShirt.Location = new System.Drawing.Point(446, 216);
-            this.saveShirt.Name = "saveShirt";
-            this.saveShirt.Size = new System.Drawing.Size(94, 29);
-            this.saveShirt.TabIndex = 12;
-            this.saveShirt.Text = "SAVE";
-            this.saveShirt.UseVisualStyleBackColor = true;
-            // 
-            // savePant
-            // 
-            this.savePant.Location = new System.Drawing.Point(446, 270);
-            this.savePant.Name = "savePant";
-            this.savePant.Size = new System.Drawing.Size(94, 29);
-            this.savePant.TabIndex = 13;
-            this.savePant.Text = "SAVE";
-            this.savePant.UseVisualStyleBackColor = true;
-            // 
-            // saveJacket
-            // 
-            this.saveJacket.Location = new System.Drawing.Point(446, 330);
-            this.saveJacket.Name = "saveJacket";
-            this.saveJacket.Size = new System.Drawing.Size(94, 29);
-            this.saveJacket.TabIndex = 14;
-            this.saveJacket.Text = "SAVE";
-            this.saveJacket.UseVisualStyleBackColor = true;
-            // 
-            // saveHat
-            // 
-            this.saveHat.Location = new System.Drawing.Point(446, 390);
-            this.saveHat.Name = "saveHat";
-            this.saveHat.Size = new System.Drawing.Size(94, 29);
-            this.saveHat.TabIndex = 15;
-            this.saveHat.Text = "SAVE";
-            this.saveHat.UseVisualStyleBackColor = true;
-            // 
             // saveShoe
             // 
-            this.saveShoe.Location = new System.Drawing.Point(446, 456);
+            this.saveShoe.Location = new System.Drawing.Point(414, 33);
             this.saveShoe.Name = "saveShoe";
             this.saveShoe.Size = new System.Drawing.Size(94, 29);
             this.saveShoe.TabIndex = 16;
             this.saveShoe.Text = "SAVE";
             this.saveShoe.UseVisualStyleBackColor = true;
+            this.saveShoe.Click += new System.EventHandler(this.saveShoe_Click);
             // 
             // inventoryBox
             // 
             this.inventoryBox.BackColor = System.Drawing.SystemColors.Window;
-            this.inventoryBox.Location = new System.Drawing.Point(12, 518);
+            this.inventoryBox.Location = new System.Drawing.Point(18, 410);
             this.inventoryBox.Name = "inventoryBox";
-            this.inventoryBox.Size = new System.Drawing.Size(528, 29);
+            this.inventoryBox.Size = new System.Drawing.Size(1027, 29);
             this.inventoryBox.TabIndex = 17;
             // 
             // viewButton
             // 
-            this.viewButton.Location = new System.Drawing.Point(583, 518);
+            this.viewButton.Location = new System.Drawing.Point(449, 442);
             this.viewButton.Name = "viewButton";
             this.viewButton.Size = new System.Drawing.Size(149, 29);
             this.viewButton.TabIndex = 18;
             this.viewButton.Text = "VIEW INVENTORY";
             this.viewButton.UseVisualStyleBackColor = true;
+            this.viewButton.Click += new System.EventHandler(this.viewButton_Click);
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(348, 602);
+            this.exitButton.Location = new System.Drawing.Point(945, 501);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(94, 29);
             this.exitButton.TabIndex = 19;
             this.exitButton.Text = "EXIT";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // saveHat
+            // 
+            this.saveHat.Location = new System.Drawing.Point(414, 33);
+            this.saveHat.Name = "saveHat";
+            this.saveHat.Size = new System.Drawing.Size(94, 29);
+            this.saveHat.TabIndex = 15;
+            this.saveHat.Text = "SAVE";
+            this.saveHat.UseVisualStyleBackColor = true;
+            this.saveHat.Click += new System.EventHandler(this.saveHat_Click);
+            // 
+            // saveJacket
+            // 
+            this.saveJacket.Location = new System.Drawing.Point(413, 31);
+            this.saveJacket.Name = "saveJacket";
+            this.saveJacket.Size = new System.Drawing.Size(94, 29);
+            this.saveJacket.TabIndex = 14;
+            this.saveJacket.Text = "SAVE";
+            this.saveJacket.UseVisualStyleBackColor = true;
+            this.saveJacket.Click += new System.EventHandler(this.saveJacket_Click);
+            // 
+            // savePant
+            // 
+            this.savePant.Location = new System.Drawing.Point(414, 33);
+            this.savePant.Name = "savePant";
+            this.savePant.Size = new System.Drawing.Size(94, 29);
+            this.savePant.TabIndex = 13;
+            this.savePant.Text = "SAVE";
+            this.savePant.UseVisualStyleBackColor = true;
+            this.savePant.Click += new System.EventHandler(this.savePant_Click);
+            // 
+            // saveShirt
+            // 
+            this.saveShirt.Location = new System.Drawing.Point(413, 35);
+            this.saveShirt.Name = "saveShirt";
+            this.saveShirt.Size = new System.Drawing.Size(94, 29);
+            this.saveShirt.TabIndex = 12;
+            this.saveShirt.Text = "SAVE";
+            this.saveShirt.UseVisualStyleBackColor = true;
+            this.saveShirt.Click += new System.EventHandler(this.saveShirt_Click);
+            // 
+            // shirtsBox
+            // 
+            this.shirtsBox.Controls.Add(this.shirtQuest);
+            this.shirtsBox.Controls.Add(this.shirtAns);
+            this.shirtsBox.Controls.Add(this.saveShirt);
+            this.shirtsBox.Location = new System.Drawing.Point(12, 150);
+            this.shirtsBox.Name = "shirtsBox";
+            this.shirtsBox.Size = new System.Drawing.Size(513, 68);
+            this.shirtsBox.TabIndex = 20;
+            this.shirtsBox.TabStop = false;
+            // 
+            // pantsBox
+            // 
+            this.pantsBox.Controls.Add(this.pantQuest);
+            this.pantsBox.Controls.Add(this.pantAns);
+            this.pantsBox.Controls.Add(this.savePant);
+            this.pantsBox.Location = new System.Drawing.Point(11, 224);
+            this.pantsBox.Name = "pantsBox";
+            this.pantsBox.Size = new System.Drawing.Size(514, 68);
+            this.pantsBox.TabIndex = 21;
+            this.pantsBox.TabStop = false;
+            // 
+            // jacketsBox
+            // 
+            this.jacketsBox.Controls.Add(this.jacketQuest);
+            this.jacketsBox.Controls.Add(this.jacketAns);
+            this.jacketsBox.Controls.Add(this.saveJacket);
+            this.jacketsBox.Location = new System.Drawing.Point(12, 298);
+            this.jacketsBox.Name = "jacketsBox";
+            this.jacketsBox.Size = new System.Drawing.Size(513, 68);
+            this.jacketsBox.TabIndex = 22;
+            this.jacketsBox.TabStop = false;
+            // 
+            // hatsBox
+            // 
+            this.hatsBox.Controls.Add(this.hatQuest);
+            this.hatsBox.Controls.Add(this.hatAns);
+            this.hatsBox.Controls.Add(this.saveHat);
+            this.hatsBox.Location = new System.Drawing.Point(531, 150);
+            this.hatsBox.Name = "hatsBox";
+            this.hatsBox.Size = new System.Drawing.Size(514, 68);
+            this.hatsBox.TabIndex = 23;
+            this.hatsBox.TabStop = false;
+            // 
+            // shoesBox
+            // 
+            this.shoesBox.Controls.Add(this.showQuest);
+            this.shoesBox.Controls.Add(this.shoeAns);
+            this.shoesBox.Controls.Add(this.saveShoe);
+            this.shoesBox.Location = new System.Drawing.Point(531, 224);
+            this.shoesBox.Name = "shoesBox";
+            this.shoesBox.Size = new System.Drawing.Size(514, 68);
+            this.shoesBox.TabIndex = 24;
+            this.shoesBox.TabStop = false;
             // 
             // clothingInventoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 643);
+            this.ClientSize = new System.Drawing.Size(1059, 542);
+            this.Controls.Add(this.shoesBox);
+            this.Controls.Add(this.hatsBox);
+            this.Controls.Add(this.jacketsBox);
+            this.Controls.Add(this.pantsBox);
+            this.Controls.Add(this.shirtsBox);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.viewButton);
             this.Controls.Add(this.inventoryBox);
-            this.Controls.Add(this.saveShoe);
-            this.Controls.Add(this.saveHat);
-            this.Controls.Add(this.saveJacket);
-            this.Controls.Add(this.savePant);
-            this.Controls.Add(this.saveShirt);
-            this.Controls.Add(this.shoeAns);
-            this.Controls.Add(this.hatAns);
-            this.Controls.Add(this.jacketAns);
-            this.Controls.Add(this.pantAns);
-            this.Controls.Add(this.shirtAns);
-            this.Controls.Add(this.showQuest);
-            this.Controls.Add(this.hatQuest);
-            this.Controls.Add(this.pantQuest);
-            this.Controls.Add(this.jacketQuest);
-            this.Controls.Add(this.shirtQuest);
             this.Controls.Add(this.inventoryQuestion);
             this.Controls.Add(this.inventoryIntro);
             this.Name = "clothingInventoryForm";
             this.Text = "Clothing Inventory";
+            this.shirtsBox.ResumeLayout(false);
+            this.shirtsBox.PerformLayout();
+            this.pantsBox.ResumeLayout(false);
+            this.pantsBox.PerformLayout();
+            this.jacketsBox.ResumeLayout(false);
+            this.jacketsBox.PerformLayout();
+            this.hatsBox.ResumeLayout(false);
+            this.hatsBox.PerformLayout();
+            this.shoesBox.ResumeLayout(false);
+            this.shoesBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -278,14 +350,19 @@ namespace InventoryProgram
         private System.Windows.Forms.TextBox jacketAns;
         private System.Windows.Forms.TextBox hatAns;
         private System.Windows.Forms.TextBox shoeAns;
-        private System.Windows.Forms.Button saveShirt;
-        private System.Windows.Forms.Button savePant;
-        private System.Windows.Forms.Button saveJacket;
-        private System.Windows.Forms.Button saveHat;
         private System.Windows.Forms.Button saveShoe;
         private System.Windows.Forms.Label inventoryBox;
         private System.Windows.Forms.Button viewButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button saveHat;
+        private System.Windows.Forms.Button saveJacket;
+        private System.Windows.Forms.Button savePant;
+        private System.Windows.Forms.Button saveShirt;
+        private System.Windows.Forms.GroupBox shirtsBox;
+        private System.Windows.Forms.GroupBox pantsBox;
+        private System.Windows.Forms.GroupBox jacketsBox;
+        private System.Windows.Forms.GroupBox hatsBox;
+        private System.Windows.Forms.GroupBox shoesBox;
     }
 }
 
--- InventoryProgram/Form1.cs
+++ InventoryProgram/Form1.cs
@@ -10,12 +10,95 @@ using System.Windows.Forms;
 
 namespace InventoryProgram
 {
-    public partial class clothingInventoryForm : Form
+    public partial class clothingInventoryForm : Form // inventory class
     {
         public clothingInventoryForm()
         {
             InitializeComponent();
         }
+        public Boolean shirtInventory = false; //boolean for if shirt box is filled out
+        public Boolean pantInventory = false; // boolean for if pants box is filled out
+        public Boolean jacketInventory = false; // boolean for if jacket box is filled out
+        public Boolean hatInventory = false; //boolean for if hate box is filled out
+        public Boolean shoeInventory = false; //boolean for if shoe box is filled out
 
+        private void saveShirt_Click(object sender, EventArgs e)
+        {
+            //when user presses save it turns the boolean statement to true
+            shirtInventory = true;
+        }
+
+        private void savePant_Click(object sender, EventArgs e)
+        {
+            //when user presses save it turns the boolean statement to true
+            pantInventory = true;
+        }
+
+        private void saveJacket_Click(object sender, EventArgs e)
+        {
+            //when user presses save it turns the boolean statement to true
+            jacketInventory = true;
+        }
+
+        private void saveHat_Click(object sender, EventArgs e)
+        {
+            //when user presses save it turns the boolean statement to true
+            hatInventory = true;
+        }
+
+        private void saveShoe_Click(object sender, EventArgs e)
+        {
+            //when user presses save it turns the boolean statement to true
+            shoeInventory = true;
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+            //once all boxes are filled out and saved
+            if (shirtInventory == true && pantInventory == true && jacketInventory == true
+                && hatInventory == true && shoeInventory == true)
+            {
+                int shirts; //amount of shirts
+                int pants; //amount of pants
+                int jackets; //amount of jackets
+                int hats; //amount of hats
+                int shoes; //amount of shoes
+                int totalInventory; //sum of all inventory items
+
+                try
+                {
+                    //tries to convert user input into an integer and save each item
+                    shirts = int.Parse(shirtAns.Text);
+                    pants = int.Parse(pantAns.Text);
+                    jackets = int.Parse(jacketAns.Text);
+                    hats = int.Parse(hatAns.Text);
+                    shoes = int.Parse(shoeAns.Text);
+
+                    //adds all items together and saves as total inventory
+                    totalInventory = shirts + pants + jackets + hats + shoes;
+
+                    //displays all inventory and total inventory
+                    inventoryBox.Text = "You have " + shirts + " shirts, " + pants + "  pairs of pants, " +
+                        jackets + " jackets, " + hats + " hats, and " + shoes +
+                        " pairs of shoes, making your total inventory " + totalInventory + " pieces of clothing.";
+                }
+                catch
+                {
+                    //if any of the boxes are not filled with an integer and cannot be converted
+                    MessageBox.Show("Please make sure all answers are a valid integer...");
+                }
+            }
+            else
+            {
+                //if user did not save all boxes
+                MessageBox.Show("Please fill out all inventory boxes and press save...");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes program
+            this.Close();
+        }
     }
 }
